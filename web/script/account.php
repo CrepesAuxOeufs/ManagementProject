9bfb703adef160b3f81eb007c2783be01765c1c8
Blackout76 <olivier.leger@imerir.com>
2015-11-13 15:18:08 +0100

    Added function getAllUser with param + Base post/get

--- web/script/account.php
+++ web/script/account.php
@@ -1,12 +1,56 @@
 <?php
+	include 'message.php';
+	include 'connect.php';
 	/* 
 	*/
+
+	/* ###########################
+	* 			MAIN ACTION
+	*  ########################### */
+	$connexion = connectBDD();
+	$json = json_decode(file_get_contents("php://input"),true);
+	$request = $json["request"];
+	$dataResponse = null;
+	
+	if($request ==  "getAllAccount"){
+		$dataResponse = getUserList($json["raw"]);
+	}
+	
+	if($dataResponse == null)
+		$response = getJSONFromCodeError(202);
+	else{
+		$response = getJSONFromCodeError(200);
+		$response["data"] = $dataResponse;
+	}
+	echo json_encode($response);
+	
+	
+	/* ###########################
+	* 			FUNCTIONS
+	*  ########################### */
 	
 	function getUserList($data){
-	
+		$raw = "";
+		foreach ($data as $value)
+			$raw = $raw . $value .',';
+		$raw = substr($raw,0,-1);
+		
+        $result = mysql_query("SELECT ". $raw ." FROM USER");
+		$users = array();
+        while($row = mysql_fetch_assoc($result))
+        {
+			$userInfo = array();
+			foreach ($data as $value)
+				$userInfo[$value] = $row[$value];
+			array_push($users, $userInfo);
+		}
+		
+		return $users;
 	}
 	
 	function createUser($data){
 	
 	}
+	
+	
 ?>
