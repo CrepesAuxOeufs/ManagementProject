6825789bfec1acb87ff33be4119c34d2425d8a6f
cedricmenet <ced.menet@gmail.com>
2015-11-13 16:45:15 +0100

    LoadModulePage

--- web/js/Login.js
+++ web/js/Login.js
@@ -1,7 +1,14 @@
 var ObligatoryLogin = true;
+var AutoHide = true;
+var loadDashboard = true;
 
 
 if(ObligatoryLogin)		$('#myModal').modal({backdrop:'static',keyboard:false, show:true});
+
+if(AutoHide) 		{setInterval(function(){ $('#myModal').modal('hide');},2000);}
+
+if(loadDashboard) {$('#page-wrapper>div>div').load("module/dashboardAdmin.html");}
+
 
 
 
@@ -12,7 +19,7 @@ $("#LoginSubmit").click(function() {
 	var arr = { login: $("#Loginlogin").value, password: $("#Loginpassword").value }
 	$.ajax(	{
 			type: "POST",
-			url: "../controller/account.php",
+			url: "../controller/login.php",
 			data: JSON.stringify(arr),
 			contentType: 'application/json; charset=utf-8',
 			dataType: 'json',
