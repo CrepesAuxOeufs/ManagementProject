4bf4e1204f77920c3dc28d230f7d7faac1a97eee
Blackout76 <olivier.leger@imerir.com>
2015-11-13 11:41:10 +0100

    Added connect.php with fucntion connectBDD()

---
+++ web/script/connect.php
@@ -0,0 +1,21 @@
+<?php
+	/* 
+	* connexion: connectBDD()
+	* disable avertissment message for mysql in the future depreciated : ini_set('display_errors','off') // on off
+	*/
+	function connectBDD(){
+		ini_set('display_errors','off');
+		$connexion = mysql_connect("mysql.imerir.com", "parcevaux3a05","dharV6DSSx88x5MK");
+		mysql_select_db("parcevaux3a05", $connexion);
+		mysql_set_charset("utf8", $connexion);
+		
+		/*
+        $result=mysql_query("SELECT * FROM USER");
+        while($row = mysql_fetch_assoc($result))
+        {
+			echo $row['id'] . '> ' . $row['name'] . ' ' . $row['nickname'] . '  ( ' . $row['mail'] . ' )<br>';
+		}*/
+		ini_set('display_errors','on');
+	}
+	
+?>
