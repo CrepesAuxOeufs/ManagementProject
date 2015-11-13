e566ef0713f74494e62c771e35c5a1bc97d370c9
Blackout76 <olivier.leger@imerir.com>
2015-11-13 11:05:22 +0100

    Added session file with functions: createSession, destroySession and isConnected

---
+++ web/script/session.php
@@ -0,0 +1,30 @@
+<?php
+	/* 
+	* Create session (login): createSession(5,"Sarah","CROCHE","sarah.croche@imerir.com");
+	* Destroy current session (logout) : destroySession();
+	* Test is user online : isConnected();
+	*/
+	session_start(); 
+		
+	function createSession($id,$name,$nickName,$mail){
+		$_SESSION['id'] = $id;
+		$_SESSION['name'] = $name;
+		$_SESSION['nickName'] = $nickName;
+		$_SESSION['mail'] = $mail;
+		$_SESSION['connect'] = true;
+		//echo 'Session created ' . $_SESSION['name'] .  ' ' . $_SESSION['nickName'] . '!<br>';
+	}
+	
+	function destroySession(){
+		$_SESSION['connect'] = false;
+		session_destroy();
+		//echo 'Session destroyed !<br>'
+	}
+	
+	function isConnected(){
+		if($_SESSION != null && $_SESSION['connect'] != null  && $_SESSION['connect'])
+			return true;
+		else
+			return false;
+	}
+?>
