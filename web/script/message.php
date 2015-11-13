b2a296e202789096b10cab764df9bcc34871297b
Blackout76 <olivier.leger@imerir.com>
2015-11-13 12:09:54 +0100

    FIX message.php

--- web/script/message.php
+++ web/script/message.php
@@ -1,12 +1,25 @@
 <?php
 	/* 
+	* create json response with code error
 	*/
 	
-	function getUserList($data){
+	function getJSONFromCodeError($code){
 	
+		$enum_message = array(
+			200  => "ok",
+			201  => "cant connect bdd"
+		);
+	
+		if($code == 200)
+			$success = true;
+		else 
+			$success = false;
+			
+		$response = json_encode(array(
+				"success" => $success, 
+				"code" => $code,
+				"message" => $enum_message[$code]));
+		return $response;
 	}
 	
-	function createUser($data){
-	
-	}
 ?>
