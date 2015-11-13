77efc35232fd6ba0ba57b823414466e18f3c823d
Blackout76 <olivier.leger@imerir.com>
2015-11-13 16:52:04 +0100

    Added Function save

--- web/controller/account.php
+++ web/controller/account.php
@@ -1,19 +1,102 @@
 <?php
 	include 'message.php';
 	include 'connect.php';
+	
 	/* 
 	*/
+	
+	$jsonString = '	{
+					"request": "save",
+					"data": {
+						"belbin": [
+							{
+								"name": "president",
+								"value": "0"
+							},
+							{
+								"name": "coequipier",
+								"value": "0"
+							},
+							{
+								"name": "eclaireur",
+								"value": "0"
+							},
+							{
+								"name": "faiseur",
+								"value": "0"
+							},
+							{
+								"name": "organisateur",
+								"value": "0"
+							},
+							{
+								"name": "evaluateur",
+								"value": "0"
+							},
+							{
+								"name": "creatif",
+								"value": "0"
+							},
+							{
+								"name": "finisseur",
+								"value": "0"
+							}
+						],
+						"skills": [
+							{
+								"name": "web",
+								"value": "0"
+							},
+							{
+								"name": "bdd",
+								"value": "0"
+							},
+							{
+								"name": "programmation",
+								"value": "0"
+							},
+							{
+								"name": "metier",
+								"value": "0"
+							},
+							{
+								"name": "marketing",
+								"value": "0"
+							}
+						],
+						"incompatibility": [
+							{
+								"id": "0"
+							},
+							{
+								"id": "1"
+							},
+							{
+								"id": "2"
+							},
+							{
+								"id": "3"
+							}
+						]
+					}
+				}';
 
 	/* ###########################
 	* 			MAIN ACTION
 	*  ########################### */
 	$connexion = connectBDD();
-	$json = json_decode(file_get_contents("php://input"),true);
+	
+	//$json = json_decode(file_get_contents("php://input"),true);
+	
+	$json = json_decode($jsonString,true);
 	$request = $json["request"];
 	$dataResponse = null;
 	
 	if($request ==  "getAllAccount"){
 		$dataResponse = getUserList($json["raw"]);
+	}
+	else if($request ==  "save"){
+		$dataResponse = saveUser($json["data"]);
 	}
 	
 	if($dataResponse == null)
@@ -49,15 +132,26 @@
 	}
 	
 	function createUser($data){
-		$name=$data['name'];
-		$nickname=$data['nickname'];
-		$mail=$data['mail'];
-        $passwd=$data['password'];
-		
-		mysql_query("INSERT INTO `USER`(`name`, `nickname`, `mail`, `password`) VALUES ('". $name ."','".$nickname."','". $mail ."','".$passwd."')");
-		
 	
 	}
 	
 	
+	function saveUser($data){
+		//BelBin
+		foreach ($data["belbin"] as $belbin){ // president,coequipier,eclaireur,faiseur,organisateur,evaluateur,creatif,finisseur
+			$id_belbin = mysql_query ("SELECT * FROM BELBIN WHERE name = '" . $belbin["name"] . "'");
+			mysql_query ("INSERT INTO USER_BELBIN(user_id, belbin_id,value) VALUES ('" . getSessionID() . "','" . $id_belbin . "','" . $belbin["value"] . "')");
+		}
+		//Skills
+		foreach ($data["skills"] as $skill){ // web,bdd,programmation,metier,marketing
+			$id_skill = mysql_query ("SELECT * FROM SKILL WHERE name = '" . $skill["name"] . "'");
+			mysql_query ("INSERT INTO USER_SKILL(user_id, skill_id,value) VALUES ('" . getSessionID() . "','" . $id_skill . "','" . $skill["value"] . "')");
+		}
+		//Incompatibility
+		foreach ($data["incompatibility"] as $user_id_uncompatibility){ 
+			mysql_query ("INSERT INTO USER_UNCOMPATIBILITY(user_id, user_id_uncompatibility) VALUES ('" . getSessionID() . "','" . $user_id_uncompatibility ."')");
+		}
+		
+		return "";
+	}
 ?>
