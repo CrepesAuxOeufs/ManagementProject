618be951177c57f987a9e616041f5ae206c70461
Blackout76 <olivier.leger@imerir.com>
2015-11-16 11:53:23 +0100

    Added auto close alert + TEST login ok when: mdp/login correct or incorrect + Added empty mail or empty password

--- web/js/Login.js
+++ web/js/Login.js
@@ -1,5 +1,5 @@
 var ObligatoryLogin = true;
-var AutoHide = true;
+var AutoHide = false;
 var loadDashboard = true;
 var loadUserListe = true;
 
@@ -17,44 +17,69 @@ if(loadUserListe) {$('#contentContainer').load("module/listegroup.html");}
 $("#LoginSubmit").click(function() {
 //twitter bootstrap script
 	
-		
-	var arr = { login: $("#Loginlogin").value, password: $("#Loginpassword").value }
+	if(document.getElementById("Loginlogin").value == "" || document.getElementById("Loginlogin").value == ""){
+				logingHeaderAddMessage("erreur : Email ou mot de passe non rempli", "warning");
+				return;
+	}
+	
+	var arr = { login: document.getElementById("Loginlogin").value, password: document.getElementById("Loginpassword").value };
+	
 	$.ajax(	{
 			type: "POST",
 			url: "../controller/login.php",
 			data: JSON.stringify(arr),
-			contentType: 'application/json; charset=utf-8',
 			dataType: 'json',
 			success: function(msg){
 				console.log(msg);
-				logingHeaderAddMessage(msg,"success");
-				
 				if(msg["success"] == true){
-					setInterval(function()	{
-												$('#myModal').modal('hide');
-											},2000);
-					
+					setInterval(function()	{$('#myModal').modal('hide'); },2000);
+					logingHeaderAddMessage("Vous êtes maintenant connecté","success");
 				}
-				
+				else
+					logingHeaderAddMessage("erreur (" + msg.code + "): " + msg.message , "warning");
 			},
 			error: function(jqXHR, textStatus, errorThrown){
-				console.log(jqXHR);
-				console.log(textStatus);
-				console.log(errorThrown);
-				
 				logingHeaderAddMessage("erreur : " + jqXHR["status"]+" " + errorThrown , "warning");
-				
 			}
 	});
 	
 });
+
 //type info warning danger success
 function logingHeaderAddMessage(msg,type){
-	
-	$("#loginHeader").append('<div class="alert alert-'+type+' alert-dismissable" style="margin:5px 5px 0px 5px;"> \
+	var uuid = generateUUID();
+	console.log(uuid);
+	$("#loginHeader").append('<div class="alert alert-'+type+' alert-dismissable" id="' + uuid+ '" style="margin:5px 5px 0px 5px;"> \
 	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
 	'+msg+'. </div>');
-				
+	setInterval(function()	{document.getElementById(uuid).remove();},5000);
+}
+
+function generateUUID(){
+    var d = new Date().getTime();
+    if(window.performance && typeof window.performance.now === "function"){
+        d += performance.now();; //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+function getSession (){
+	$.ajax(	{
+			type: "POST",
+			url: "../controller/session_req.php",
+			success: function(msg){
+				console.log(msg);
+				return msg;
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				return null;
+			}
+	});
 }
 
 
