4ed4ffb6e1d9a810a9a61bebe5efca13c305712f
Blackout76 <olivier.leger@imerir.com>
2015-11-18 09:37:05 +0100

    Added belbin, uncompatibility, skills for users info request  + FIX admin not returned in user list

--- web/controller/account.php
+++ web/controller/account.php
@@ -4,84 +4,6 @@
 	include 'session.php';
 	
 	error_reporting(0);
-	/* 
-	*/
-	
-	$jsonString = '	{
-					"request": "save",
-					"data": {
-						"belbin": [
-							{
-								"name": "president",
-								"value": "0"
-							},
-							{
-								"name": "coequipier",
-								"value": "1"
-							},
-							{
-								"name": "eclaireur",
-								"value": "0"
-							},
-							{
-								"name": "faiseur",
-								"value": "10"
-							},
-							{
-								"name": "organisateur",
-								"value": "0"
-							},
-							{
-								"name": "evaluateur",
-								"value": "3"
-							},
-							{
-								"name": "creatif",
-								"value": "0"
-							},
-							{
-								"name": "finisseur",
-								"value": "0"
-							}
-						],
-						"skills": [
-							{
-								"name": "web",
-								"value": "0"
-							},
-							{
-								"name": "bdd",
-								"value": "6"
-							},
-							{
-								"name": "programmation",
-								"value": "3"
-							},
-							{
-								"name": "metier",
-								"value": "1"
-							},
-							{
-								"name": "marketing",
-								"value": "0"
-							}
-						],
-						"incompatibility": [
-							{
-								"id": "0"
-							},
-							{
-								"id": "1"
-							},
-							{
-								"id": "2"
-							},
-							{
-								"id": "3"
-							}
-						]
-					}
-				}';
 
 	/* ###########################
 	* 			MAIN ACTION
@@ -117,18 +39,89 @@
 	
 	function getUserList($data){
 		$raw = "";
+		$belbin = false;
+		$skills = false;
+		$uncompatibility = false;
 		foreach ($data as $value)
-			$raw = $raw . $value .',';
+			if($value == "belbin")
+				$belbin = true;
+			else if($value == "skills")
+				$skills = true;
+			else if($value == "uncompatibility")
+				$uncompatibility = true;
+			else
+				$raw = $raw . $value .',';
 		$raw = substr($raw,0,-1);
 		
-        $result = mysql_query("SELECT ". $raw ." FROM USER");
+        $result = mysql_query("SELECT ". $raw ." FROM USER WHERE admin = 0");
 		$users = array();
         while($row = mysql_fetch_assoc($result))
         {
 			$userInfo = array();
 			foreach ($data as $value)
-				$userInfo[$value] = $row[$value];
+				if($value != "belbin" && $value != "skills" && $value != "uncompatibility")
+					$userInfo[$value] = $row[$value];
+					
+			//BELBIN
+			if($belbin){
+				$belbins = array();
+				$resultBelbin = mysql_query( "SELECT USER.id,USER.admin, USER.name,USER.nickname,BELBIN.id as belbin_id, USER_BELBIN.value as belbin_value,BELBIN.name as belbin_name
+										FROM `USER`
+										INNER JOIN `USER_BELBIN` ON USER.id = USER_BELBIN.user_id
+										INNER JOIN `BELBIN` ON BELBIN.id  =  USER_BELBIN.belbin_id
+										WHERE USER.id = '". $userInfo["id"] . "'");
+
+				while($rowBelbin = mysql_fetch_assoc($resultBelbin))
+				{
+					$belbin = array();
+					$belbin["id"] = $rowBelbin["belbin_id"];
+					$belbin["value"] = $rowBelbin["belbin_value"];
+					$belbin["name"] = $rowBelbin["belbin_name"];
+					array_push($belbins, $belbin);
+				}
+				//print_r($belbins);
+				$userInfo["belbin"] = $belbins;
+			}
+			
+			//SKILL
+			if($skills){
+				$skills = array();
+				$resultSkill = mysql_query( "SELECT USER.id,USER.name,USER.nickname,SKILL.id as skill_id, USER_SKILL.value as skill_value, SKILL.name as skill_name
+											FROM `USER`
+											INNER JOIN `USER_SKILL` ON USER.id =USER_SKILL.user_id
+											INNER JOIN `SKILL` ON SKILL.id  =  USER_SKILL.skill_id
+											WHERE USER.id = '". $userInfo["id"] . "'");
+
+				while($rowSkill = mysql_fetch_assoc($resultSkill))
+				{
+					$skill = array();
+					$skill["id"] = $rowSkill["skill_id"];
+					$skill["value"] = $rowSkill["skill_value"];
+					$skill["name"] = $rowSkill["skill_name"];
+					array_push($skills, $skill);
+				}
+				//print_r($skills);
+				$userInfo["skills"] = $skills;
+			}
+			//UNCOMPATIBILITY
+			if($uncompatibility){
+				$uncompatibilities = array();
+				$resultUncompatibility = mysql_query( "SELECT USER.id,USER.name,USER.nickname, USER_UNCOMPATIBILITY.user_id_uncompatibility
+											FROM `USER`
+											INNER JOIN `USER_UNCOMPATIBILITY` ON USER.id =USER_UNCOMPATIBILITY.user_id
+											WHERE USER.id = '". $userInfo["id"] . "'");
+
+				while($rowUncompatibility = mysql_fetch_assoc($resultUncompatibility))
+				{
+					$uncompatibility = array();
+					$uncompatibility["id"] = $rowUncompatibility["user_id_uncompatibility"];
+					array_push($uncompatibilities, $uncompatibility);
+				}
+				$userInfo["uncompatibility"] = $uncompatibilities;
+			}
+		
 			array_push($users, $userInfo);
+			
 		}
 		
 		if(	$users == null)
