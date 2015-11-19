92e9d32c93b070fd5292776ef1619d2b2d1becf1
Blackout76 <olivier.leger@imerir.com>
2015-11-19 12:24:05 +0100

    Connect to local BDD

--- web/controller/connect.php
+++ web/controller/connect.php
@@ -6,8 +6,8 @@
 	error_reporting(0);
 	function connectBDD(){
 		ini_set('display_errors','off');
-		//$connexion = mysql_connect("mysql.imerir.com", "parcevaux3a05","dharV6DSSx88x5MK");
-		$connexion = mysql_connect("127.0.0.1", "root","");
+		$connexion = mysql_connect("mysql.imerir.com", "parcevaux3a05","dharV6DSSx88x5MK");
+		//$connexion = mysql_connect("127.0.0.1", "root","");
 		mysql_select_db("parcevaux3a05", $connexion);
 		mysql_set_charset("utf8", $connexion);
 		/*
